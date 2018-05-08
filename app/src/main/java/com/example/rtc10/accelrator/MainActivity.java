15d177eed880369bedf384c3a75b1da8552fe744
ych861031 <ych861031@gmail.com>
2018-05-08 13:43:01 +0800

    change think

--- app/src/main/java/com/example/rtc10/accelrator/MainActivity.java
+++ app/src/main/java/com/example/rtc10/accelrator/MainActivity.java
@@ -7,6 +7,8 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.lang.reflect.Type;
 
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private SensorManager sensorManager;
     private Sensor mAccelerometer;
+    TextView textView;
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
 
     }
 
@@ -36,25 +43,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
     }
 
+
     float x = 0;
     float y = 0;
+    float  max=0;
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+
+
         if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            if (x!=event.values[0]||y!=event.values[1]){
-                Log.e("eventX",String.valueOf(event.values[0]));
-                Log.e("eventY",String.valueOf(event.values[1]));
-                Log.e("eventZ",String.valueOf(event.values[2]));
-                x=event.values[0];
-                y=event.values[1];
+
+            if (Math.abs(event.values[0])>max){
+                max = event.values[0];
             }
+            Log.e("eventX",String.valueOf(event.values[0]));
+            textView.setText(String.valueOf(event.values[0]));
+
+
         }
+
+
+
 
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         Log.e("accuracy",String.valueOf(accuracy));
+    }
+
+    int i=0;
+    public void fn_start(View v){
+        if (i==0){
+            max=0;
+            x = 0;
+            i++;
+            textView2.setText("0");
+        }else {
+            i=0;
+            textView2.setText(String.valueOf(max));
+        }
+
     }
 }
